d976d78db08d77b9f7116d1a65d8289acaa1445d
Stanislav Petrov <catnislav@gmail.com>
2021-11-24 11:58:14 +0100

    updated storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,17 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y"
+  ],
+  "typescript": {
+    reactDocgen: 'react-docgen'
+  },
+  "webpackFinal": (config) => {
+    delete config.resolve.alias['emotion-theming'];
+    delete config.resolve.alias['@emotion/styled'];
+    delete config.resolve.alias['@emotion/core'];
+    return config;
+  },
 }
